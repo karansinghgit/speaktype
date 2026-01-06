f806513a8791bac688f4d57fdfb40eeb69333e66
Karan Singh <karansingh1559@gmail.com>
2026-01-07 03:37:17 +0530

    feat: initialize Xcode project

---
+++ speaktype/App/speaktypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  speaktypeApp.swift
+//  speaktype
+//
+//  Created by Karan Singh on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct speaktypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ speaktypeTests/speaktypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  speaktypeTests.swift
+//  speaktypeTests
+//
+//  Created by Karan Singh on 7/1/26.
+//
+
+import Testing
+@testable import speaktype
+
+struct speaktypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
